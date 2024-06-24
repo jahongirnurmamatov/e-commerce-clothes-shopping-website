eaaca1b6b9bc4f721cf4d1f308ba10f6dfa597fc
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-06-24 23:41:23 +0500

    shopcategory page is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ import ShopCategory from './pages/ShopCategory';
 import LoginSignup from './pages/LoginSignup';
 import Cart from './pages/Cart';
 import Footer from './components/footer/Footer';
+import men_banner  from './components/assets/banner_mens.png'
+import women_banner  from './components/assets/banner_women.png'
+import kids_banner  from './components/assets/banner_kids.png'
 
 function App() {
   return (
@@ -17,9 +20,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>} />
-        <Route  path='/men' element={<ShopCategory category='men'/>}/>
-        <Route  path='/women' element={<ShopCategory category='women'/>}/>
-        <Route  path='/kids' element={<ShopCategory category='kids'/>}/>
+        <Route  path='/men' element={<ShopCategory banner={men_banner} category='men'/>}/>
+        <Route  path='/women' element={<ShopCategory banner={women_banner} category='women'/>}/>
+        <Route  path='/kids' element={<ShopCategory banner={kids_banner} category='kid'/>}/>
         <Route path='/product' element={<Product/>}>
         <Route path='"productId' element={<Product/>}/>
         </Route>

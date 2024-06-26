e73dcd4270815bc2f48953a2d87eca0a40d7b1a6
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-06-26 12:40:36 +0500

    Sidebar to admin panel

---
+++ admin/src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+import navlogo from '../../assets/nav-logo.svg';
+import navProfileIcon from '../../assets/nav-profile.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={navlogo} alt="" className="nav-logo" />
+        <img src={navProfileIcon}  className='nav-profile' alt="" />
+    </div>
+  )
+}
+
+export default Navbar
---
+++ admin/src/pages/admin/Admin.jsx
@@ -0,0 +1,12 @@
+import Sidebar from '../../components/sidebar/Sidebar'
+import './Admin.css'
+
+const Admin = () => {
+  return (
+    <div className='admin'>
+         <Sidebar/>
+    </div>
+  )
+}
+
+export default Admin
